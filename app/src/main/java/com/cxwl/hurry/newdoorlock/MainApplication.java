1fc7ff70205d6986ec92f9412e22a59eac1b473d
StoneXL <3401794305@qq.com>
2018-05-30 11:12:51 +0800

    奔溃重启

--- app/src/main/java/com/cxwl/hurry/newdoorlock/MainApplication.java
+++ app/src/main/java/com/cxwl/hurry/newdoorlock/MainApplication.java
@@ -22,10 +22,10 @@ import okhttp3.OkHttpClient;
  * Created by William on 2018/4/26.
  */
 
-public class MainApplication  extends Application  implements Thread.UncaughtExceptionHandler{
+public class MainApplication  extends Application  {
 
     private static MainApplication application;
-
+    PendingIntent restartIntent;
     @Override
     public void onCreate() {
         application = this;
@@ -45,7 +45,23 @@ public class MainApplication  extends Application  implements Thread.UncaughtExc
         OkHttpUtils.initClient(okHttpClient);
         //初始化腾讯buggly
         CrashReport.initCrashReport(this, "4e8a21b88b", true);
+        Intent intent = new Intent();
+        // 参数1：包名，参数2：程序入口的activity
+        intent.setClassName("com.cxwl.hurry.newdoorlock", "com.cxwl.hurry.newdoorlock.ui.activity.MainActivity");
+        restartIntent = PendingIntent.getActivity(getApplicationContext(), 0,
+                intent, Intent.FLAG_ACTIVITY_NEW_TASK);
+        Thread.setDefaultUncaughtExceptionHandler(restartHandler); // 程序崩溃时触发线程
     }
+
+    public Thread.UncaughtExceptionHandler restartHandler = new Thread.UncaughtExceptionHandler() {
+        @Override
+        public void uncaughtException(Thread thread, Throwable ex) {
+            AlarmManager mgr = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+            mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 1000,
+                    restartIntent); // 1秒钟后重启应用
+            android.os.Process.killProcess(android.os.Process.myPid()); // 自定义方法，关闭当前打开的所有avtivity
+        }
+    };
     static DaoSession mDaoSessin;
     public static DaoSession getGreenDaoSession() {
         if (mDaoSessin == null) {
@@ -58,23 +74,5 @@ public class MainApplication  extends Application  implements Thread.UncaughtExc
     public static MainApplication getApplication() {
         return application;
     }
-    // 捕获系统运行停止错误
-    @Override
-    public void uncaughtException(Thread thread, Throwable ex) {
-        // System.exit(0);
 
-        Intent intent = getBaseContext().getPackageManager()
-                .getLaunchIntentForPackage(getBaseContext().getPackageName());
-
-        PendingIntent restartIntent = PendingIntent.getActivity(
-                getApplicationContext(),
-                0, intent, Intent.FLAG_ACTIVITY_NEW_TASK);
-
-        // 退出程序
-        AlarmManager mgr = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-        mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 500, restartIntent); // 1秒钟后重启应用
-
-        System.exit(0);
-
-    }
 }
